e39170d817851cebb052d0b231223749e0570b3d
stopaalex <stopa.alex@gmail.com>
2018-05-11 13:47:10 -0400

    UPDATE :::
    new user... add images... load screen... essentially fully functional to create and log into (but not out of) a profile

--- scripts.js
+++ scripts.js
@@ -1,9 +1,23 @@
+// MAIN JS PAGE FOR THE SOCIAL NETWORK
+
+// // // // // // // // // // // /
+// CONSTANTS AND VARIABLES // //
+// // // // // // // // // // /
 const goHomeBtn = document.querySelector('#goHome');
-const logInCont = document.querySelector('#feedLoginContent');
-const splashContent = document.querySelector('#feedWelcomeContent');
-const newPostHTML = '<div class="feed-add"><div class="input"><textarea id="feedAddContent"></textarea></div><div class="post-to-feed"><button id="postToFeed">Post to feed</button></div></div>';
+const logInCont = document.querySelector('#welcomeLoginContent');
+const splashContent = document.querySelector('#welcomeWelcomeContent');
 const createProfileModal = document.querySelector('#createProfileModal');
 const saveLogInConfirm = document.querySelector('#saveLogInConfirm');
+const welcomeNoAuth = document.querySelector('.home-welcome-not-authenticated');
+
+const cornerProfImg = document.querySelector('.welcome-corner-profile');
+const welcomeAuthed = document.querySelector('.home-welcome-authenticated');
+const welcomeUserContainer = document.querySelector('.welcome-user-container');
+const feedContainter = document.querySelector('.feed-container');
+const welcomeUserContent = document.querySelector('#welcomeUserContent');
+const feedContent = document.querySelector('#feedContent');
+
+
 var database,
     storage,
     users = [],
@@ -12,9 +26,14 @@ var database,
     userLoggedIn = false,
     userHasSavedCreds = false;
 
-// Hord coded in just to test functionality - not secure at all!!!
-var password = '';
+// // // // // // /
+// FUNCTIONS // //
+// // // // // //
 
+/**
+ * @name initializeFirebase
+ * @desc creating the initial firebase database connection
+*/
 function initializeFirebase() {
     var config = {
         apiKey: "AIzaSyDmhOnSLgpxFDryaKyK3mAEhXdXu6MLvsc",
@@ -52,6 +71,22 @@ function getUserInfo() {
 }
 
 /**
+ * @name UpdateUserInfo
+ * @desc updates the users array to ensure all user data is accounted for.
+*/
+function updateUserInfo() {
+    users = [];
+    var ref = database.ref("users");
+
+    ref.once("value", function (snapshot) {
+        snapshot.forEach(function (childSnapshot) {
+            // console.log(childSnapshot.val());
+            users.push(childSnapshot.val());
+        });
+    });
+}
+
+/**
  * @name checkForSavedCreds
  * @desc checks the local storage for saved credentials and then auto logs in
  */
@@ -72,40 +107,16 @@ function autoLogIn(credentials) {
     users.forEach(function (user) {
         if (credentials.lastName === user.last_name && credentials.password === user.pass) {
             userLoggedIn = true;
-            document.querySelector('#feedAddContent').innerHTML = newPostHTML;
+            activeUser = user.unique_ID;
+            // document.querySelector('#feedAddContent').innerHTML = newPostHTML;
             logInCont.style.display = 'none';
         }
     });
     if (userLoggedIn) {
-        getPostsForFeed();
+        logInCont.style.display = 'none';
+        splashContent.style.display = 'none';
+        getLoggedInUserInfo();
     }
-}
-
-/**
- * @name getPostsForFeed
- * @desc gets all of the posts for the feed
-*/
-function getPostsForFeed() {
-    posts = [];
-    // Defining the reference to get data from the database
-    var ref = database.ref("Posts");
-    ref.on("value", function (snapshot) {
-        snapshot.forEach(function (childSnapshot) {
-            posts.push(childSnapshot.val());
-        });
-        var feedHTML = posts.map(function (post) {
-            var poster = post.poster;
-            var text_content = post.text_content;
-            return '<div class="feed-post"><div class="info">' + poster + '</div><div class="text">' + text_content + '</div></div>';
-        }).join('');
-
-        if (userLoggedIn) {
-            document.querySelector('#feedAddContent').innerHTML = newPostHTML;
-            document.querySelector('#feedContent').innerHTML = feedHTML;
-            logInCont.style.display = 'none';
-            splashContent.style.display = 'none';
-        }
-    });
 }
 
 /**
@@ -148,7 +159,7 @@ function createNewUserProfile() {
         console.log('PASSWORDS DONT MATCH');
         alertTextContainer.textContent = '* Passwords Do Not Match';
         return;
-    // CHECK FOR CONTENTS IN FIRST, LAST, PASS, AND PASSCHECK BEFORE UPLOADING
+        // CHECK FOR CONTENTS IN FIRST, LAST, PASS, AND PASSCHECK BEFORE UPLOADING
     } else if (!firstName || !lastName || !password || !passwordCheck) {
         console.log('MISSING DATA');
         alertTextContainer.textContent = '* Your\'re Missing Info';
@@ -179,24 +190,61 @@ function createNewUserProfile() {
             storageRef.put(pictureFile);
         }
 
-        // CHECK TO ENSURE NEW USER WAS CREATED AND THEN CLEAR INPUTS
-        users.forEach(function (user) {
-            if (user.unique_ID === uniqueID) {
-                // CLEAR OUT VALUES
-                firstName = '';
-                lastName = '';
-                password = '';
-                passwordCheck = '';
-                picturePath = null;
-                pictureFile = null;
-                // HIDE THE MODAL
-                createProfileModal.style.display = 'none';
-                alertTextContainer.textContent = null;
-                console.log('PROFILE CREATED');
-            }
+        users = [];
+        database.ref("users").once("value", function (snapshot) {
+            snapshot.forEach(function (childSnapshot) {
+                // console.log(childSnapshot.val());
+                users.push(childSnapshot.val());
+            });
+
+            logInFromCreation(lastName, password);
+
+            users.forEach(function (user) {
+                if (user.unique_ID === uniqueID) {
+                    // CLEAR OUT VALUES
+                    firstName = '';
+                    lastName = '';
+                    password = '';
+                    passwordCheck = '';
+                    picturePath = null;
+                    pictureFile = null;
+                    // HIDE THE MODAL
+                    createProfileModal.style.display = 'none';
+                    alertTextContainer.textContent = null;
+                    console.log('PROFILE CREATED');
+                }
+            });
         });
+
+        // CHECK TO ENSURE NEW USER WAS CREATED AND THEN CLEAR INPUTS
     }
 }
+
+/**
+ * @name logInFromCreation
+ * @desc logs the user in after creating a profile
+ * @param {string} lastName 
+ * @param {string} password 
+ */
+function logInFromCreation(lastName, password) {
+    users.forEach(function (user) {
+        if (lastName === user.last_name && password === user.pass) {
+            userLoggedIn = true;
+            activeUser = user.unique_ID;
+            // document.querySelector('#feedAddContent').innerHTML = newPostHTML;
+            logInCont.style.display = 'none';
+        }
+    });
+    if (userLoggedIn) {
+        logInCont.style.display = 'none';
+        splashContent.style.display = 'none';
+        console.log('LOGGED IN');
+        getLoggedInUserInfo();
+    } else {
+        console.log('BAD CREDENTIALS');
+    }
+}
+
 /**
  * @name userLogIn
  * @desc log in from the user
@@ -205,17 +253,19 @@ function userLogIn() {
     var lastName, password;
     lastName = document.querySelector('#logInLastName').value;
     password = document.querySelector('#logInPassword').value;
-    debugger
     users.forEach(function (user) {
         if (lastName === user.last_name && password === user.pass) {
             userLoggedIn = true;
-            document.querySelector('#feedAddContent').innerHTML = newPostHTML;
+            activeUser = user.unique_ID;
+            // document.querySelector('#feedAddContent').innerHTML = newPostHTML;
             logInCont.style.display = 'none';
         }
     });
     if (userLoggedIn) {
         console.log('LOGGED IN');
-        getPostsForFeed();
+        logInCont.style.display = 'none';
+        splashContent.style.display = 'none';
+        getLoggedInUserInfo();
     } else {
         console.log('BAD CREDENTIALS');
     }
@@ -245,8 +295,79 @@ function updateLogInSave() {
     return checkbox.checked;
 }
 
+/**
+ * @name getLoggedInUserInfo
+ * @desc once the user has been logged get the new content - types of log ins - auto(localstorage saved creds), user(entering name and password), creation(after creating a new profile)
+*/
+function getLoggedInUserInfo() {
+
+    welcomeNoAuth.style.display = 'none';
+    welcomeAuthed.style.display = 'flex';
+
+    updateUserInfo();
+    setTimeout(function() {
+        createWelcomeContent();
+    }, 1000);
+    // createWelcomeContent();
+    // welcomeUserContent.innerHTML = createWelcomeContent();
+
+    getPostsForFeed();
+}
+
+function createWelcomeContent() {
+    var activeUserInfo = {
+        firstName: '',
+        lastName: '',
+        imgLocation: ''
+    }
+        
+    users.forEach(function(user) {
+        if (activeUser === user.unique_ID) {
+            activeUserInfo = {
+                firstName: user.first_name,
+                lastName: user.last_name,
+                imgLocation: 'https://firebasestorage.googleapis.com/v0/b/socialnetwork-6ff89.appspot.com/o/' + user.first_name + '_' + user.last_name + '_' + user.unique_ID + '?alt=media&token=2133d104-6d2b-419c-b9d5-93c3bbdac05f',
+            }
+        }
+    });
+    cornerProfImg.style.display = 'flex';
+    cornerProfImg.innerHTML = '<div class="user-name">' + activeUserInfo.firstName + '</div><div class="user-img"><img src="' + activeUserInfo.imgLocation + '"/><div>'
+    welcomeUserContent.innerHTML = '<div>Welcome to the SOCIALnetwork ' + activeUserInfo.firstName+ '!</div>';
+
+    // console.log(activeUserInfo);
+}
+
+/**
+ * @name getPostsForFeed
+ * @desc gets all of the posts for the feed
+*/
+function getPostsForFeed() {
+    posts = [];
+    // Defining the reference to get data from the database
+    var ref = database.ref("Posts");
+    ref.once("value", function (snapshot) {
+        snapshot.forEach(function (childSnapshot) {
+            posts.push(childSnapshot.val());
+        });
+        // var feedHTML = posts.map(function (post) {
+        //     var poster = post.poster;
+        //     var text_content = post.text_content;
+        //     return '<div class="feed-post"><div class="info">' + poster + '</div><div class="text">' + text_content + '</div></div>';
+        // }).join('');
+
+        // if (userLoggedIn) {
+        //     document.querySelector('#feedAddContent').innerHTML = newPostHTML;
+        //     document.querySelector('#feedContent').innerHTML = feedHTML;
+        // }
+    });
+}
+
 function initialize() {
+    // DEFUALT HIDE THE CREATE PROFILE
     createProfileModal.style.display = 'none';
+    // DEFUALT HIDE THE AUTHENTICATED USER CONTENT
+    welcomeAuthed.style.display = 'none';
+    cornerProfImg.style.display = 'none';
 
     initializeFirebase();
 
