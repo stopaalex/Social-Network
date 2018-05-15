70321b10f7f7d57b72d80ba9c6fb9a43fb145d84
stopaalex <stopa.alex@gmail.com>
2018-05-15 12:44:26 -0400

    UPDATE :::
    profile... posts show up in profile -- need about and other stuff now

--- profile.js
+++ profile.js
@@ -1,5 +1,6 @@
 var email,
     users,
+    posts,
     selectedProfile;
 
 function getEmail() {
@@ -50,9 +51,39 @@ function getUserInfo() {
 
         selectedProfile.pictue = 'https://firebasestorage.googleapis.com/v0/b/socialnetwork-6ff89.appspot.com/o/' + selectedProfile.first_name + '_' + selectedProfile.last_name + '_' + selectedProfile.unique_ID + '?alt=media&token=2133d104-6d2b-419c-b9d5-93c3bbdac05f';
 
-        // console.log(selectedProfile);
+        createProfileHeader();
+        createProfilePosts();
 
     });
+}
+
+function createProfileHeader() {
+    let profileImg = document.querySelector('#profileImg');
+    let profileName = document.querySelector('#profileName');
+    profileImg.innerHTML = '<img src="' + selectedProfile.pictue + '"/>';
+    profileName.textContent = selectedProfile.first_name + ' ' + selectedProfile.last_name;
+}
+
+function createProfilePosts() {
+    posts = [];
+    var ref = database.ref("Posts");
+
+    ref.once("value", function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+            var post = childSnapshot.val();
+            if (post.poster.email === selectedProfile.email) {
+                posts.push(post);
+            }
+        });
+
+        let postsContainer = document.querySelector('#profilePosts');
+
+        var postHTML = posts.map(function(post) {
+            return '<div class="profile-post"><div class="profile-post-date">' + post.date_string + '</div><div class="profile-post-text">' + post.text_content + '</div></div>'
+        }).join('');
+
+        postsContainer.innerHTML = postHTML;
+    })
 }
 
 function initialize() {
